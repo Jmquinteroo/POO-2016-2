2c8cd7bbeb09151f9e06e3b98d9a7a6aa5db4788
Jmquinteroo <jmquinteroo@unal.edu.co>
2016-09-12 21:55:31 -0500

    sd
    asd

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,28 +2,12 @@ class HolaMundo {
 
  public static void main(String[] args) {
  
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 sout("hola mundo")
-Luisa es fea
-asdsads
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-System.out.println("hola mundo");
-=======
-systen.out.println("hola mundo")
->>>>>>> origin/master
-=======
-System.out.println("hola mundo");
->>>>>>> origin/master
->>>>>>> origin/master
-=======
+
 
 System.out.println("hola mundo");
-
->>>>>>> origin/master
-
+System.out.println("hola mundo");
 }
 }
 
